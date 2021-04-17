aead6f20430f8270b0bc5454e21e9ab63e497a61
Taylan YILDIZ <taylanyildz@outlook.com>
2021-04-17 23:04:08 +0300

    added user

---
+++ lib/models/user_model.dart
@@ -0,0 +1,21 @@
+class User {
+  final int user_id;
+  final String user_realname;
+  final String user_name;
+  final String user_bio;
+  final String user_birthdate;
+  final String user_photo_1;
+  final String user_photo_2;
+  final String user_photo_3;
+
+  User({
+    this.user_id,
+    this.user_realname,
+    this.user_name,
+    this.user_bio,
+    this.user_birthdate,
+    this.user_photo_1,
+    this.user_photo_2,
+    this.user_photo_3,
+  });
+}
